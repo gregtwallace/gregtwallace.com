ad1c2f12a32e5671476c3a153573ba7a405b6dbc
Greg T. Wallace <greg@gregtwallace.com>
2021-06-12 19:47:06 -0400

    Fix "Warning Prop `href` did not match"

--- components/UI/Navbar/NavbarContent/NavDropdown/NavDropdown.js
+++ components/UI/Navbar/NavbarContent/NavDropdown/NavDropdown.js
@@ -1,19 +1,17 @@
-import Link from 'next/link';
-
 const NavDropdown = (props) => {
   return (
     <li className='nav-item dropdown'>
-      <Link href='#'>
-        <a
-          className='nav-link dropdown-toggle'
-          role='button'
-          data-toggle='dropdown'
-          aria-haspopup='true'
-          aria-expanded='false'
-        >
-          {props.name}
-        </a>
-      </Link>
+      <a
+        className='nav-link dropdown-toggle'
+        href='#'
+        role='button'
+        data-toggle='dropdown'
+        aria-haspopup='true'
+        aria-expanded='false'
+      >
+        {props.name}
+      </a>
+
       <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
         {props.children}
       </div>
