a0c3a15a89c9c4cd26c7e9294bf0887bcb083bcd
Greg T. Wallace <greg@gregtwallace.com>
2021-06-14 15:49:42 -0400

    Remove redundant charset

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,6 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <Fragment>
       <Head>
-        <meta charset='utf-8'></meta>
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
