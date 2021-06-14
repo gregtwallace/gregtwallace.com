79f49a68f5a3974547d4e22222ee3523b834a0b7
Greg T. Wallace <greg@gregtwallace.com>
2021-06-14 16:46:37 -0400

    Breakdown donate into components

--- pages/donate/index.js
+++ pages/donate/index.js
@@ -1,10 +1,14 @@
+import { Fragment } from 'react';
 import Head from 'next/head';
 
 import Jumbotron from '../../components/UI/Jumbotron';
+import Row from '../../components/UI/Layout/Row';
+import ColumnThird from '../../components/UI/Layout/ColumnThird';
+import CardWithImage from '../../components/UI/Layout/CardWithImage';
 
 const DonatePage = () => {
   return (
-    <>
+    <Fragment>
       <Head>
         <title>Greg T. Wallace</title>
         <meta
@@ -15,67 +19,59 @@ const DonatePage = () => {
       </Head>
 
       <Jumbotron title='Donate'>
-        Help cover the costs of hosting this site, or just buy me a beer to say thanks.
+        Help cover the costs of hosting this site, or just buy me a beer to say
+        thanks.
       </Jumbotron>
 
-      <div className='row'>
-        <div className='col-sm-4'>
-          <div className='card h-100'>
-            <img
-              className='card-img-top'
-              src='/images/donate/zelle_logo.webp'
-            />
-            <div className='card-body'>
-              <h5>Zelle</h5>
-              <p>No fees & instant delivery.</p>
-              <p>
-                <strong>Send via Zelle App to:</strong> greg@gregtwallace.com
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className='col-sm-4'>
-          <div className='card h-100'>
-            <img
-              className='card-img-top'
-              src='/images/donate/venmo_logo.webp'
-            />
-            <div className='card-body'>
-              <h5>Venmo</h5>
-              <p>No fees.</p>
-              <a
-                href='https://venmo.com/gtwallace'
-                className='btn btn-primary'
-                target='_blank'
-              >
-                Venmo Me
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className='col-sm-4'>
-          <div className='card h-100'>
-            <img
-              className='card-img-top'
-              src='/images/donate/paypal_logo.webp'
-            />
-            <div className='card-body'>
-              <h5>Paypal</h5>
-              <p>
-                They steal part of the money, but I'll still be very grateful!
-              </p>
-              <a
-                href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=44ZHKU4BWDUKN'
-                className='btn btn-primary'
-                target='_blank'
-              >
-                PayPal Me
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      <Row>
+        <ColumnThird>
+          <CardWithImage
+            imageSrc='/images/donate/zelle_logo.webp'
+            imageAlt='Zelle logo.'
+            title='Zelle'
+          >
+            <p>No fees & instant delivery.</p>
+            <p>
+              <strong>Send via Zelle App to:</strong> greg@gregtwallace.com
+            </p>
+          </CardWithImage>
+        </ColumnThird>
+
+        <ColumnThird>
+          <CardWithImage
+            imageSrc='/images/donate/venmo_logo.webp'
+            imageAlt='Venmo logo.'
+            title='Venmo'
+          >
+            <p>No fees.</p>
+            <a
+              href='https://venmo.com/gtwallace'
+              className='btn btn-primary'
+              target='_blank'
+            >
+              Venmo Me
+            </a>
+          </CardWithImage>
+        </ColumnThird>
+
+        <ColumnThird>
+          <CardWithImage
+            imageSrc='/images/donate/paypal_logo.webp'
+            imageAlt='PayPal logo.'
+            title='PayPal'
+          >
+            <p>They steal part of the money, but I'll still be very grateful!</p>
+            <a
+              href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=44ZHKU4BWDUKN'
+              className='btn btn-primary'
+              target='_blank'
+            >
+              PayPal Me
+            </a>
+          </CardWithImage>
+        </ColumnThird>
+      </Row>
+    </Fragment>
   );
 };
 
