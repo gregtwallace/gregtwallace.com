551a571b8f4ed553b0af0887a8be334beb01baa0
Greg T. Wallace <greg@gregtwallace.com>
2021-06-14 16:10:34 -0400

    Adjust footer

--- components/UI/PageSections/AlwaysFooter.js
+++ components/UI/PageSections/AlwaysFooter.js
@@ -1,17 +1,15 @@
 const AlwaysFooter = () => {
   return (
     <footer className='container'>
-      <div className='footer-copyright text-center py-3'>
+      <div className='text-center py-3'>
         <hr />
         <p>© 2019-21 Greg T. Wallace</p>
-        <p>
-          <a
-            href='https://policies.google.com/technologies/partner-sites'
-            target='_blank'
-          >
-            Google Data Use Policy
-          </a>
-        </p>
+        <a
+          href='https://policies.google.com/technologies/partner-sites'
+          target='_blank'
+        >
+          Google Data Use Policy
+        </a>
       </div>
     </footer>
   );
