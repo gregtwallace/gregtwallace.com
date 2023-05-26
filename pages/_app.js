b0a48669c9f491174b5ec22b78bc96135c2fa730
Greg T. Wallace <greg@gregtwallace.com>
2023-05-25 23:52:16 -0400

    move stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -30,15 +30,15 @@ const MyApp = ({ Component, pageProps }) => {
           content='https://www.gregtwallace.com/images/og/og_image.png'
         />
         <meta property='og:url' content='https://www.gregtwallace.com/' />
-
-        {/* TODO: custom css? */}
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css'
-          integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
-          crossOrigin='anonymous'
-        />
       </Head>
+
+      {/* TODO: custom css? */}
+      <link
+        rel='stylesheet'
+        href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css'
+        integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
+        crossOrigin='anonymous'
+      />
 
       <GoogleAdsHead />
 
